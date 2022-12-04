18c1196fd98e7eed4462fd3a318e3e61ea95915b
Sachin Zade <sachinzade1992@gmail.com>
2022-12-04 22:34:32 +0530

    initial commit

---
+++ src/components/directory/directory.component.js
@@ -0,0 +1,12 @@
+import CategoryItem from "../cateogry-item/cateogry-item.component";
+import "./directory.styles.scss";
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+export default Directory;
